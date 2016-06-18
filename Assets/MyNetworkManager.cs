fea8d5261f4a58410b8fe8668ca20acea7baf026
Junaid <messagejunaid@gmail.com>
2016-06-18 00:57:05 -0400

    Re Organize Assets

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+
+    public void MyStartHost()
+    {
+        base.StartHost();
+        Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+    }
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+
+        Debug.Log("Host Started at " + Time.timeSinceLevelLoad);
+    }
+}
