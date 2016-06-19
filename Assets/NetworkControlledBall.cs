f9ab094e2172ffc5eccee9edf71ba3795a88237a
Junaid <messagejunaid@gmail.com>
2016-06-19 17:32:55 -0400

    Switched player to ball for testing

---
+++ Assets/NetworkControlledBall.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Ball;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class NetworkControlledBall : NetworkBehaviour {
+
+    private Ball ball; // Reference to the ball controller.
+
+    private Vector3 move;
+    // the world-relative desired move direction, calculated from the camForward and user input.
+
+    private Vector3 camForward; // The current forward direction of the camera
+    private bool jump; // whether the jump button is currently pressed
+
+
+    private void Awake()
+    {
+        // Set up the reference.
+        ball = GetComponent<Ball>();
+        
+    }
+
+
+    private void Update()
+    {
+        // Get the axis and jump input.
+        if(isLocalPlayer)
+        {
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            jump = CrossPlatformInputManager.GetButton("Jump");
+
+            // calculate move direction
+            move = (v * Vector3.forward + h * Vector3.right).normalized;
+        }
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Call the Move function of the ball controller
+        ball.Move(move, jump);
+        jump = false;
+    }
+}
